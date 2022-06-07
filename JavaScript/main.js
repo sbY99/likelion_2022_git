893fe6c8914e2d2a9c5030fc9befbd5ed0a310e2
sbY99 <didtmdqlsdl@gmail.com>
2022-06-07 22:48:57 +0900

    init

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,4 @@
+/*
 document.getElementsByClassName('animal')[0].style.color = 'blue';
 document.querySelectorAll('.animal')[1].style.color = 'green';
 document.querySelectorAll('.animal')[2].innerHTML = 'dog';
@@ -11,3 +12,37 @@ document.querySelectorAll('.box')[0].classList.remove('purple');
 
 document.querySelectorAll('.box')[0].classList.toggle('yellow');
 document.querySelectorAll('.box')[0].classList.toggle('yellow');
+
+var num =0;
+document.getElementById('plus').addEventListener('click', function(){
+    num++;
+    document.getElementById('num').innerHTML = num;
+});
+
+document.getElementById('minus').addEventListener('click', function(){
+    num--;
+    document.getElementById('num').innerHTML = num;
+});
+
+document.querySelector('.bar').addEventListener('click',function(){
+    document.querySelector('.bar').innerHTML = 'Clicked!';
+    document.querySelector('.newBar').classList.toggle('show') = 'block';
+});
+*/
+
+document.querySelectorAll('.list')[0].addEventListener('click',function(e){
+    tabBtn(e.target.dataset.id);
+})
+
+
+function tabBtn(a){
+    document.querySelectorAll('.tab-button')[a].addEventListener('click',function(){
+        for(let i =0; i<document.querySelectorAll('.tab-button').length; i++){
+            document.querySelectorAll('.tab-button')[i].classList.remove('here');
+            document.querySelectorAll('.tab-content')[i].classList.remove('show');
+        }
+        document.querySelectorAll('.tab-button')[a].classList.add('here');
+        document.querySelectorAll('.tab-content')[a].classList.add('show');
+        
+    });
+}
